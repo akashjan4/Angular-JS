2cee1c0586f4857d1fdcfd82d28223424a53880c
Akash Rastogi <akashjan4@gmail.com>
2015-04-18 21:53:20 +0530

    on text selected event
    when text is selects in text area pop up will show with the selected
    text

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
     var app = angular.module('store', []);
     app.controller('StoreContoller', function() {
         this.products = gems;
+       
+         
     });
     var gems = [
     {
@@ -15,7 +17,23 @@
                 full: 'img/ic_plus_circle_outline_black_48dp.png',
                 thumb: 'img/ic_plus_circle_outline_black_18dp.png'
 
-            }]
+            }],
+        reviews:[
+            {star:5,
+                body:"this is awesome",
+                author:"@jadkskd"
+            },
+        {star:5,
+            body:"this is awesome",
+            author:"@ffgdkskd"
+        },
+        {star:5,
+            body:"this is awesome",
+            author:"@jadk454545skd"
+        },{star:5,
+            body:"this is awesome",
+            author:"@ju7u7989adkskd"
+        }]
     },
     {
         name: 'Decadoden',
@@ -28,7 +46,24 @@
                full: 'img/ic_pause_circle_outline_black_48dp.png',
                thumb: 'img/ic_pause_circle_outline_black_18dp.png'
 
-           }]
+           }],
+        reviews:[
+           {star:4,
+               body:"this is awesome",
+               author:"@jadkskd"
+           },
+       {star:3,
+           body:"this is awesome",
+           author:"@ffgdkskd"
+       },
+       {star:2,
+           body:"this is awesome",
+           author:"@jadk454545skd"
+       },{star:1,
+           body:"this is awesome",
+           author:"@ju7u7989adkskd"
+       }]
+        
     },
     {
         name: 'Pentadoden',
@@ -41,7 +76,24 @@
                full: 'img/ic_play_circle_outline_black_48dp.png',
                thumb: 'img/ic_play_circle_outline_black_18dp.png'
 
-           }]
+           }],
+        reviews:[
+           {
+               star:2,
+               body:"this is awesome",
+               author:"@jadkskd"
+           },
+       {star:2,
+           body:"this is awesome",
+           author:"@ffgdkskd"
+       },
+       {star:5,
+           body:"this is awesome",
+           author:"@jadk454545skd"
+       },{star:6,
+           body:"this is awesome",
+           author:"@ju7u7989adkskd"
+       }]
 
     }];
     app.controller('PanelController', function () {
@@ -51,6 +103,31 @@
         };
         this.isSelected = function (checkTab) {
             return this.tab === checkTab;
+        };
+        this.getText = function () {
+            var selText = "";
+            if (window.getSelection) {  
+                if (document.activeElement &&
+                        (document.activeElement.tagName.toLowerCase() == "textarea" ||
+                         document.activeElement.tagName.toLowerCase() == "input")) {
+                    var text = document.activeElement.value;
+                    selText = text.substring(document.activeElement.selectionStart,
+                                              document.activeElement.selectionEnd);
+                }
+                else {
+                    var selRange = window.getSelection();
+                    selText = selRange.toString();
+                }
+            }
+            else {
+                if (document.selection.createRange) { // Internet Explorer
+                    var range = document.selection.createRange();
+                    selText = range.text;
+                }
+            }
+            if (selText !== "") {
+                alert(selText);
+            }
         };
     });
 
